25cec4f78b5761426c2dbb166bd0ee2af97b78e3
Nick Wose <nickmwise@gmail.com>
2017-08-07 17:18:32 -0700

    Add user authentication and link to database.. Also Set up a Scurity for displaying pages based on valid credential loggin info.

---
+++ src/BasicAuthentication/ViewModels/LoginViewsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicAuthentication.ViewModels
+{
+    public class LoginViewsModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
